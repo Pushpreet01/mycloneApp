03a9b10236b0ab09045af92e846ec3475fb801c5
kellyokada <kelly.okada.p@gmail.com>
2025-01-22 17:16:24 -0700

    add header, bio and stats

--- myCloneApp/App.tsx
+++ myCloneApp/App.tsx
@@ -1,17 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
       {/* Header */}
       <View style={styles.header}>
         <View style={styles.profileSection}>
-          <Image 
-            source={require('./assets/profile_picture.jpeg')} 
-            style={styles.profileImage} 
+          <Image
+            source={require('./assets/profile_picture.jpeg')}
+            style={styles.profileImage}
           />
         </View>
         <View style={styles.statsSection}>
@@ -30,56 +29,63 @@ export default function App() {
         </View>
       </View>
 
-        {/* Bio */}
-        <View style={styles.bioSection}>
-          <Text style={styles.bioBold}>OOTD Everyday</Text>
-          <Text style={styles.bioText}>fit check!</Text>
-          <Text style={styles.bioText}>you know we'll hype you up.</Text>
+      {/* Bio */}
+      <View style={styles.bioSection}>
+        <Text style={styles.bioBold}>OOTD Everyday</Text>
+        <Text style={styles.bioText}>fit check!</Text>
+        <Text style={styles.bioText}>you know we'll hype you up.</Text>
+      </View>
+
+      {/* Member Button */}
+      <View>
+        <TouchableOpacity onPress={() => Alert.alert('Member', 'Member Button pressed')}>
+          <Text style={styles.member_text}>
+            member <Image source={require('./assets/down-arrow.png')} style={{ height: 18, width: 18 }} />
+          </Text>
+        </TouchableOpacity>
+
+        {/* Grid */}
+        <View style={styles.photo_grid}>
+          <Image source={require('./assets/gridPhotos/testimg1.jpg')} style={styles.photo_grid_size} />
+          <Image source={require('./assets/gridPhotos/testimg2.jpg')} style={styles.photo_grid_size} />
+          <Image source={require('./assets/gridPhotos/testimg3.jpg')} style={styles.photo_grid_size} />
         </View>
-
-
+        <View style={styles.photo_grid}>
+          <Image source={require('./assets/gridPhotos/testimg4.jpg')} style={styles.photo_grid_size} />
+          <Image source={require('./assets/gridPhotos/testimg5.jpg')} style={styles.photo_grid_size} />
+          <Image source={require('./assets/gridPhotos/testimg6.jpg')} style={styles.photo_grid_size} />
+        </View>
+        <View style={styles.photo_grid}>
+          <Image source={require('./assets/gridPhotos/testimg7.jpg')} style={styles.photo_grid_size} />
+          <Image source={require('./assets/gridPhotos/testimg8.jpg')} style={styles.photo_grid_size} />
+          <Image source={require('./assets/gridPhotos/testimg9.jpg')} style={styles.photo_grid_size} />
+        </View>
+      </View>
 
       {/* Tool bar */}
       <View style={styles.toolbar}>
         <View style={styles.button}>
-          <Image 
-            source={require('./assets/home.png')} 
-            style={{ height: '60%', width: '60%' }} 
-          />
+          <Image source={require('./assets/home.png')} style={{ height: '60%', width: '60%' }} />
         </View>
         <View style={styles.button}>
-          <Image 
-            source={require('./assets/search.png')} 
-            style={{ height: '50%', width: '50%' }} 
-          />
+          <Image source={require('./assets/search.png')} style={{ height: '50%', width: '50%' }} />
         </View>
         <View style={styles.button}>
-          <Image 
-            source={require('./assets/reel.png')} 
-            style={{ height: '50%', width: '50%' }} 
-          />
+          <Image source={require('./assets/reel.png')} style={{ height: '50%', width: '50%' }} />
         </View>
         <View style={styles.button}>
-          <Image 
-            source={require('./assets/shop.png')} 
-            style={{ height: '55%', width: '55%' }} 
-          />
+          <Image source={require('./assets/shop.png')} style={{ height: '55%', width: '55%' }} />
         </View>
         <View style={styles.button}>
-          <Image 
-            source={require('./assets/profile.png')} 
-            style={{ height: '50%', width: '50%' }} 
-          />
+          <Image source={require('./assets/profile.png')} style={{ height: '50%', width: '50%' }} />
         </View>
       </View>
 
       {/* Alert Button */}
-      <TouchableOpacity 
-        style={styles.alert_button} 
-        onPress={() => Alert.alert('Alert', 'Alert Button pressed')}
-      >
+      <TouchableOpacity style={styles.alert_button} onPress={() => Alert.alert('Alert', 'Alert Button pressed')}>
         <Text style={styles.alert_text}>Alert</Text>
       </TouchableOpacity>
+
     </View>
   );
 }
@@ -87,32 +93,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'flex-start',  
+    backgroundColor: '#f9f9f9',
   },
+
   header: {
     flexDirection: 'row',
-    padding: 10,
+    paddingVertical: 20,  
+    paddingHorizontal: 10,
     alignItems: 'center',
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
     backgroundColor: '#fff',
-    height: 100, 
-    justifyContent: 'space-between', 
-    top: 100,
+    height: 220,  
+    justifyContent: 'space-between',  
   },
+
   profileSection: {
-    flex: 1,
     alignItems: 'center',
   },
+
   profileImage: {
     width: 80,
     height: 80,
     borderRadius: 40,
   },
   statsSection: {
-    flex: 2,
     flexDirection: 'row',
     justifyContent: 'space-around',
+    flex: 2,
+    marginLeft: 10,  
   },
   statBox: {
     alignItems: 'center',
@@ -127,7 +137,7 @@ const styles = StyleSheet.create({
   },
   bioSection: {
     padding: 10,
-    marginTop: 120, 
+    marginTop: 20,  
   },
   bioBold: {
     fontSize: 16,
@@ -137,6 +147,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#333',
   },
+
   toolbar: {
     flexDirection: 'row',
     justifyContent: 'space-around',
@@ -145,10 +156,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     borderTopWidth: 1,
     borderTopColor: '#ddd',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
   },
   button: {
     flex: 1,
@@ -161,15 +168,31 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     borderRadius: 5,
     margin: 10,
-    position: 'absolute',
-    bottom: 60, 
-    left: 20,
-    right: 20,
   },
   alert_text: {
     color: 'white',
     fontSize: 20,
   },
 
-});
+  member_text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#bebebe',
+    padding: 5,
+    margin: 20,
+    borderRadius: 5,
+  },
 
+  photo_grid: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    margin: 3,
+  },
+
+  photo_grid_size: {
+    height: 120,
+    width: 120,
+  },
+});
